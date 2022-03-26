2ab91e8555334da3503bc93949dfbeab238f190e
lvcfr <lvcfr923@gmail.com>
2022-03-26 01:12:03 -0700

    first

---
+++ testClass.js
@@ -0,0 +1,14 @@
+function testFunc(kind, name, age) {
+    this.kind = kind;
+    this.name = name;
+    this.age = age;
+}
+
+testFunc.prototype.getName = function () {
+    return this.name;
+}
+
+testFunc.prototype.contructor = testFunc;
+
+module.exports = testFunc;
+
---
+++ testRun.js
@@ -0,0 +1,30 @@
+const myPet = require('./testClass.js');
+const assert = require('assert');
+
+const firstPet = new myPet('dog', 'kira', 500); 
+const secondPet = new myPet('cat', 'traitor', 200); 
+
+console.log(firstPet.constructor === myPet);
+console.log(secondPet.getName());
+
+function greetings(name) {
+    const sayThis = `welcome ${name}!`;
+
+    assert(typeof name === 'string', 'wrong type!'); 
+
+    outputIt(sayThis);
+}
+
+function outputIt(phrase) {
+    console.log(phrase);
+}
+
+greetings('hello world');
+
+const reduceSampleTest = [1, 2, 3, 4, 5];
+
+const accuReduceSampleTest = reduceSampleTest
+    .reduce((prev, curr) => prev + curr, 5);
+
+console.log(accuReduceSampleTest);
+
